10e34e40c7ee394a59e067bcdcb101cc1617fa81
Toniirubilar <laboralagus@gmail.com>
2024-05-09 20:33:02 -0300

    counter

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import Navbar from './components/Navbar.jsx'
 import Principal from './Principal.jsx'
 import Footer from './components/Footer.jsx'
 import './App.css'
+import Counter from './components/Counter.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <Principal />
     <Footer />
+    <Counter />
   </React.StrictMode>,
 )
